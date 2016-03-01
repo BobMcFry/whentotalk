0809dd3c0948ea862b30ecedd1494c37aabb33b5
Christian <cityfisch@yahoo.de>
2016-03-01 12:53:33 +0100

    Added Licensing Headers to every sourcefile.

--- src/base.js
+++ src/base.js
@@ -1,3 +1,20 @@
+/*
+WhenToTalk: A software to plan conversations distributed over the world easily.
+Copyright (C) 2016  Christian Heiden
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
 
 /* Interval */
 
