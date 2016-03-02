caee4160d385982665b5f29b38d5a6105ee2f801
Christian <cityfisch@yahoo.de>
2016-03-02 16:25:47 +0100

    Put all js code together in one src file in order to reduce filetransmissions.

--- src/whentotalk.js
+++ src/whentotalk.js
@@ -29,6 +29,100 @@ PERSON_CNT 	= 4;
 /** Array of persons that participate in the conversation */
 persons 	= new Array();
 
+
+
+/** 
+ *	Interval object.
+ */
+var Interval = function(start, end, offset){
+	/** Start time of Interval */
+	this.start		= start;
+	/** End time of Interval */
+	this.end 		= end;
+	/** Offset to UTC */
+	this.offset 	= offset;
+}
+
+/** 
+ *	Returns an array of corresponding UTC intervals because of a possible split
+ *	at midnight.
+ */
+Interval.prototype.getUTCIntervals = function() {
+	var returnIntervals = new Array();
+
+	var start 	= this.start - this.offset;
+	var end 	= this.end - this.offset;
+	start 		= (start+24)%24;
+	end 		= (end+24)%24;
+	if (end == 0){
+		end = 24;
+	}
+	if (start > end){
+		// split
+		returnIntervals.push(new Interval(start, 24, this.offset));
+		returnIntervals.push(new Interval(0, end, this.offset));
+	} else {
+		// regular
+		returnIntervals.push(new Interval(start, end, this.offset));
+	}
+	
+	return returnIntervals;
+};
+
+
+/**
+ *	Person object.
+ */
+var Person = function(name, cityIdx, timeslider, resultbar, interval){
+	this.name 		= name;
+	this.cityIdx 	= cityIdx; // if -1 the person is not used
+	this.timeslider = timeslider;
+	this.resultbar	= resultbar;
+	this.interval 	= interval;
+}
+
+/**
+ *	Reads the interval of its corresponding timeslider.
+ */
+Person.prototype.readInterval = function() {
+	if (this.cityIdx < 0) {
+		this.interval 	= null;
+	} else {
+		var range 		= this.timeslider.noUiSlider.get();
+		this.interval 	= new Interval(parseInt(range[0]), parseInt(range[1]), cities[this.cityIdx].offset);
+	}
+}
+
+/**
+ *	Displays the result in the persons resultbar. Does all the conversion into
+ *	the persons own timezone. Sort of a WRApPeR, yoo!
+ */
+Person.prototype.displayResult = function(intervals) {
+	var convertedIntervals = new Array();
+	var offset = this.interval.offset;
+	for (var i = 0; i < intervals.length; i++) {
+		var newInterval = new Interval((intervals[i].start + offset + 24)%24, (intervals[i].end + offset + 24)%24, 0);
+		if (newInterval.end == 0){
+			newInterval.end = 24;
+		}
+		convertedIntervals.push(newInterval);
+	};
+	convertedIntervals = sortIntervalArray(convertedIntervals);
+	displayResult(this.resultbar, convertedIntervals);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 /** 
  * 	Initialiazation method that is called upon creation of the website.
  */
